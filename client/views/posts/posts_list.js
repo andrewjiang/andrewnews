cf60eedc0d8bb5ef674119e4b9982e47cb9de957
andrewjiang <andrew.h.jiang@gmail.com>
2013-11-18 14:47:46 -0800

    init commit

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,22 @@
+var postsData = [
+	{
+		title: 'What its like to fail',
+		author: 'Some guy',
+		url: 'http://priceonomics.com/what-its-like-to-fail/'
+	},
+	{
+		title: 'Aereo could trigger the end of free TV game broadcasts',
+		author: 'Ted Johnson',
+		url: 'http://variety.com/2013/tv/news/nfl-major-league-baseball-warn-supreme-court-that-aereo-could-trigger-end-to-games-on-free-tv-1200847089/'
+	},
+	{
+		title: 'Pentagon guilty of billion-dollar accounting fraud, reveals Reuters investigation',
+		author: 'Russell Brandom',
+		url: 'http://theverge.com/2013/11/18/5117816/pentagon-guilty-of-billion-dollar-accounting-fraud-reveals-reuters'
+	}
+];
+Template.postsList.helpers({
+	posts: function() {
+		return Posts.find();
+	}
+});
